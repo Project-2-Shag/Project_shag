9ef6701f45a9edf3680e3d3b3f28901bf1a4b1f6
NotyTrask <53178492+NotyTrask@users.noreply.github.com>
2020-02-26 19:13:37 +0300

    Initial commit

---
+++ app/src/main/java/com/shag/MainActivity.java
@@ -0,0 +1,70 @@
+package com.shag;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+
+public class MainActivity extends AppCompatActivity
+{
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
+    private EditText emailId, passwordId;
+    private Button signInButton, registrationButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        emailId = (EditText) findViewById(R.id.emailId);
+        passwordId = (EditText) findViewById(R.id.passwordId);
+        signInButton = (Button) findViewById(R.id.signInButton);
+        registrationButton = (Button) findViewById(R.id.registrationButton);
+
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = emailId.getText().toString();
+                String password = passwordId.getText().toString();
+                if (email.isEmpty())
+                {
+                    emailId.setError("Please, enter e-mail");
+                    emailId.requestFocus();
+                }
+                else if (password.isEmpty())
+                {
+                    passwordId.setError("Please, enter password");
+                    passwordId.requestFocus();
+                }
+                else if (email.isEmpty() && password.isEmpty())
+                {
+                    Toast.makeText(MainActivity.this, "Enter e-mail and password", Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    mAuth.signInWithEmailAndPassword(email, password);
+                }
+            }
+        });
+
+        registrationButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, RegistrationActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/shag/RegistrationActivity.java
@@ -0,0 +1,61 @@
+package com.shag;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+
+public class RegistrationActivity extends Activity
+{
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
+    private EditText emailId, passwordId;
+    private Button registrationButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        emailId = (EditText) findViewById(R.id.emailId);
+        passwordId = (EditText) findViewById(R.id.passwordId);
+        registrationButton = (Button) findViewById(R.id.registrationButton);
+
+        registrationButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = emailId.getText().toString();
+                String password = passwordId.getText().toString();
+                if (email.isEmpty())
+                {
+                    emailId.setError("Please, enter e-mail");
+                    emailId.requestFocus();
+                }
+                else if (password.isEmpty())
+                {
+                    passwordId.setError("Please, enter password");
+                    passwordId.requestFocus();
+                }
+                else if (email.isEmpty() && password.isEmpty())
+                {
+                    Toast.makeText(RegistrationActivity.this, "Enter e-mail and password", Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    mAuth.createUserWithEmailAndPassword(email, password);
+                }
+            }
+        });
+    }
+}
